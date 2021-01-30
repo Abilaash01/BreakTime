db237a6c9591529f95fee7a4067796793cddf8bc
Nutan Nimkar <nnimk038@uottawa.ca>
2021-01-30 18:00:37 -0500

    delete myscript.js
    we dont need it

--- myscript.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById("submit").addEventListener("click", function() {
-    document.getElementById("check").innerHTML = "Hello World";
-  })
-  
-  var element = document.getElementById("minsInterval");
-  var elemennt2 = document.getElementById("minsBreak");
